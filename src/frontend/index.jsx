b316818344bef583fe80082bce6f1bd4e1f48b90
David May <David.May@fiery.com>
2025-03-19 22:33:25 -0700

    Adding frontend invoke

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import ForgeReconciler, {Text, Button, PortalUserMenuAction, ModalDialog, Select} from '@forge/react';
+import { invoke } from '@forge/bridge';
 
 
 const App = () => {
     const [isOpen, setOpen] = useState(true);
     const [options, setOptions] = useState([]);
 
+    invoke('getRequestUrlFromFitId', { fitId: '123' }).then((response) => {
+        console.log(response);
+        setOptions(response);
+    });
+
     const handleButton = () => {
         console.log('Hello world!');
     }
+
+
 
     if (!isOpen) {
         return null;
